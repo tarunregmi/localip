ece267828baa0eb79c1be6b40c9a234310f2b0af
Tarun Regmi <tarunregmi.io@gmail.com>
2024-12-13 16:23:51 +0545

    add code to get local ip address

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::net::{SocketAddr, UdpSocket};
+
+/// Retrieves the local IP address of the current machine by connecting to a UDP socket.
+///
+/// # Returns
+/// - `Ok(String)`: The local IP address as a string.
+/// - `Err(std::io::Error)`: If there is an error binding or connecting the socket.
+pub fn get_local_ip() -> Result<String, std::io::Error> {
+    const BIND_ADDRESS: &str = "0.0.0.0:0";
+    const REMOTE_ADDRESS: &str = "192.168.0.0:0";
+
+    let socket: UdpSocket = UdpSocket::bind(BIND_ADDRESS)?;
+    socket.connect(REMOTE_ADDRESS)?;
+
+    let local_addr: SocketAddr = socket.local_addr()?;
+
+    Ok(local_addr.ip().to_string())
 }
 
 #[cfg(test)]
@@ -7,8 +22,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_get_local_ip() {
+        let local_ip: String = get_local_ip().unwrap();
+        assert!(!local_ip.is_empty());
     }
 }
